f67da87c89461ecd57d2bde9ef1bc7bed744779d
hadyhefny <hady.hefny@gmail.com>
2020-08-29 18:29:14 +0200

    learning leaders api integration

--- app/src/main/java/com/hefny/hady/gadphasetwoproject/MainActivity.kt
+++ app/src/main/java/com/hefny/hady/gadphasetwoproject/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.hefny.hady.gadphasetwoproject
 
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.hefny.hady.gadphasetwoproject.ui.main.SectionsPagerAdapter
+import com.hefny.hady.gadphasetwoproject.ui.main.SharedViewModel
+import com.hefny.hady.gadphasetwoproject.utils.Resource
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var sharedViewModel: SharedViewModel
+    private val TAG = "AppDebug"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,7 +28,28 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
+        sharedViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+            .create(SharedViewModel::class.java)
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers() {
+        sharedViewModel.learningLeadersLiveData.observe(this, Observer { dataResource ->
+            when (dataResource) {
+                is Resource.Loading -> {
+                    Log.d(TAG, "subscribeObservers: LOADING")
+                }
+                is Resource.Success -> {
+                    Log.d(TAG, "subscribeObservers: SUCCESS")
+                    Log.d(TAG, "subscribeObservers: learningLeadersList: ${dataResource.data}")
+                }
+                is Resource.Error -> {
+                    Log.d(TAG, "subscribeObservers: ERROR")
+                    Log.d(TAG, "subscribeObservers: errorMessage: ${dataResource.message}")
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/hefny/hady/gadphasetwoproject/ui/main/SharedViewModel.kt
+++ app/src/main/java/com/hefny/hady/gadphasetwoproject/ui/main/SharedViewModel.kt
@@ -1,6 +1,54 @@
 package com.hefny.hady.gadphasetwoproject.ui.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.hefny.hady.gadphasetwoproject.R
+import com.hefny.hady.gadphasetwoproject.api.ServiceGenerator
+import com.hefny.hady.gadphasetwoproject.api.responses.LearningLeader
+import com.hefny.hady.gadphasetwoproject.utils.Resource
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
-class SharedViewModel() : ViewModel() {
+class SharedViewModel(private val context: Application) : AndroidViewModel(context) {
+    private var _learningLeadersMutableLiveData =
+        MutableLiveData<Resource<ArrayList<LearningLeader>>>()
+    val learningLeadersLiveData: LiveData<Resource<ArrayList<LearningLeader>>>
+        get() = _learningLeadersMutableLiveData
+
+    init {
+        getLearningLeaders()
+    }
+
+    private fun getLearningLeaders() {
+        _learningLeadersMutableLiveData.value = Resource.Loading()
+        ServiceGenerator.getGadsApi().getLearningLeaders()
+            .enqueue(object : Callback<ArrayList<LearningLeader>> {
+                override fun onFailure(call: Call<ArrayList<LearningLeader>>, t: Throwable) {
+                    var errorMessage = context.getString(R.string.general_error_message)
+                    when (t) {
+                        is UnknownHostException, is IOException, is SocketTimeoutException -> {
+                            errorMessage = context.getString(R.string.internet_connection_error)
+                        }
+                        is HttpException -> {
+                            errorMessage = t.message()
+                        }
+                    }
+                    _learningLeadersMutableLiveData.value = Resource.Error(errorMessage)
+                }
+
+                override fun onResponse(
+                    call: Call<ArrayList<LearningLeader>>,
+                    response: Response<ArrayList<LearningLeader>>
+                ) {
+                    _learningLeadersMutableLiveData.value = Resource.Success(response.body())
+                }
+            })
+    }
 }
