5140dc0066b845b7647a1484e64b58b55301abe3
hadyhefny <hady.hefny@gmail.com>
2020-09-04 19:31:34 +0200

    splash screen logic and design

---
+++ app/src/main/java/com/hefny/hady/gadphasetwoproject/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.hefny.hady.gadphasetwoproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.WindowManager
+import androidx.lifecycle.ViewModelProvider
+import com.hefny.hady.gadphasetwoproject.ui.main.SharedViewModel
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },2000)
+    }
+}
